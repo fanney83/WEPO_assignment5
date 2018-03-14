3d2c89809af98d082a3dff87265cfc6e1c73cda1
fanney83 <fanneyv12@ru.is>
2018-03-14 09:40:54 +0000

    setup

---
+++ infinitymodules/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Modal from './components/Modal/Modal'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Modal/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ infinitymodules/src/components/Modal/Modal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Modal = () => {
+    return (
+        <p>Im a modal</p>
+    );
+};
+
+export default Modal;
