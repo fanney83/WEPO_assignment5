adf3e4dd98729f8b47b2651d26b077e6f5d9eae4
fanney83 <fanneyv12@ru.is>
2018-03-16 14:53:27 +0000

    liður 3, lookar weird samt

--- infinitymodules/src/App.js
+++ infinitymodules/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import  Modal  from './components/Modal/Modal';
-import ProgressBar from './components/ProgressBar/ProgressBar';
+//import ProgressBar from './components/ProgressBar/ProgressBar';
+import NameCard from './components/NameCard/NameCard';
 class App extends Component {
     constructor(props) {
         super(props)
@@ -12,20 +13,26 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        {!this.state.isOpen && <button onClick={() => this.setState({isOpen: true})}>Open Modal</button>}
-        <Modal
-            isOpen={this.state.isOpen}
-            onClose={() => this.setState({isOpen: false})}>
-            <Modal.Title>My Modal Title</Modal.Title>
-            <Modal.Body> My Modal Body</Modal.Body>
-            <Modal.Footer> My Modal Footer</Modal.Footer>
-        </Modal>
-        <ProgressBar
-            progress={50}
-            striped={true}
-            animated={true} />
+        <div className="App">
+            {!this.state.isOpen && <button onClick={() => this.setState({isOpen: true})}>Open Modal</button>}
+            <Modal
+                isOpen={this.state.isOpen}
+                onClose={() => this.setState({isOpen: false})}>
+                <Modal.Title>My Modal Title</Modal.Title>
+                <Modal.Body> My Modal Body</Modal.Body>
+                <Modal.Footer> My Modal Footer</Modal.Footer>
+            </Modal>
+            {/*<ProgressBar
+                progress={50}
+                striped={true}
+                animated={true} />*/}
+            <NameCard
+                name="Fanney Þóra Vilhjálmsdóttir"
+                email="fanneyv12@ru.is"
+                telephone="+354-866-5544"
+                imageUrl="https://images7.alphacoders.com/633/thumb-350-633262.png"/>
       </div>
+
     );
   }
 }
