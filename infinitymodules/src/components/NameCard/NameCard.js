d764b67e5f837c729e2acb5f5286b3773e871b69
fanney83 <fanneyv12@ru.is>
2018-03-17 19:00:26 +0000

    Lagaði modal

--- infinitymodules/src/components/NameCard/NameCard.js
+++ infinitymodules/src/components/NameCard/NameCard.js
@@ -4,7 +4,7 @@ import css from './NameCard.css';
 const NameCard = ({ name, email, telephone, imageUrl }) => {
     return (
         <div className={`${css.card}`}>
-            <div className="info">
+            <div className={`${css.info}`}>
                 <div style={{ backgroundImage: `url(${imageUrl})` }} className={`${css.imageUrl}`}></div>
                 <div className={`${css.name}`} > {name}</div>
                 <div className={`${css.email}`}> {email}</div>
